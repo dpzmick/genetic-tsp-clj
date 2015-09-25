7fa9eb8abbdd08121ac6c0651b168e2de7d3c8cd
dpzmick <dpzmick@gmail.com>
2015-09-25 01:03:27 -0500

    added parallel, still having trouble with assertions

--- src/genetic_tsp_clj/core.clj
+++ src/genetic_tsp_clj/core.clj
@@ -1,5 +1,6 @@
 (ns genetic-tsp-clj.core
   (:require [clojure.math.numeric-tower :as math]
+            [clojure.core.async :as a :refer [go >! >!! <! <!!]]
             (incanter [core :as ic]
                       [charts :as icc])))
 
@@ -22,9 +23,7 @@
 (defn crossover
   [parentA parentB]
   {:pre [(= (count parentA) (count parentB))]
-   :post [(and
-            (= (count %) (count parentA))
-            (nil? (some nil? %)))]} ;; some weird bug causes this to happen sometimes
+   :post [(nil? (some nil? %))]} ;; some weird bug causes this to happen sometimes
 
   (let
     [split-point (+ 1 (rand-int (- (count parentA) 1))) ;; random between 0-A
@@ -99,6 +98,62 @@
          (recur
            (+ 1 i)
            (build-new-population population t-size mut-rate)))))))
+
+(defn neighbors
+  [channels my-id]
+  (cond
+    (= 0 my-id)                      [(last channels) (nth channels 1)]
+    (= (- (count channels) 1) my-id) [(first channels) (nth channels (- (count channels) 2))]
+    :else                            [(nth channels (+ my-id 1)) (nth channels (- my-id 1))]))
+
+(defn worker
+  [id channels state iter-limit]
+  (a/go-loop
+    [state state
+     i 0]
+    (doseq [n (neighbors channels id)] (go (>! n state)))
+    (let
+      [incoming    (a/take 2 (nth channels id))
+       individuals (<! (a/reduce conj [state] incoming))
+       sorted      (sort-population individuals)
+       fst         (first sorted)
+       snd         (second sorted)
+       value       (crossover fst snd)]
+
+      (if (= i iter-limit)
+        value
+        (recur value (+ i 1))))))
+
+(defn solve-problem-parallel
+  [problem iter-limit pop-size mut-rate]
+  (let
+    [channels   (apply vector (take pop-size (repeatedly #(a/chan))))
+     population (make-population problem pop-size)
+     results    (map <!!
+                     (reduce
+                       #(let
+                          [channel (worker %2 channels (nth population %2) iter-limit)]
+                          (conj %1 channel))
+                       []
+                       (range pop-size)))]
+    (first (sort-population results))))
+
+(defn run-par
+  [{:keys [num-cities max-coord iter-limit pop-size mut-rate]}]
+  (let
+    [
+     problem  (generate-problem num-cities max-coord)
+     solution (solve-problem-parallel problem iter-limit pop-size mut-rate)
+     xs       (map first solution)
+     ys       (map second solution)
+     ]
+    (do
+      (println problem)
+      (def plot (icc/scatter-plot))
+      (icc/add-points plot xs ys)
+      (icc/add-lines plot xs ys)
+      (ic/view plot)
+      )))
 
 (defn run
   [{:keys [num-cities max-coord iter-limit pop-size t-size mut-rate]}]
